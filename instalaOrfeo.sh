e4e6b210d0131d077cba5ea69f14b404302bcba2
Carlos Mario Mora Restrepo <contacto@carlosmora.biz>
2017-01-07 12:59:04 -0500

    Activacion de los fragmentos comentados

--- instalaOrfeo.sh
+++ instalaOrfeo.sh
@@ -13,16 +13,16 @@ DBPASSWORD="0rf30**$$"
 INSTALLDIR="$LOCAL/orfeo384/instalacion"
 PHPDIR="/etc/php/5.6/apache2"
 ###############################################################################
-#apt-get update
-#apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
-#add-apt-repository ppa:ondrej/php; apt-get update; apt-get install php5.6-pgsql postgresql apache2 libgda-5.0-postgres   postgresql-common  postgresql-client-common libpg-perl postgresql postgresql-client php5.6 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-pgsql php5.6-xsl  php5.6-imap php5.6-sqlite3 php5.6-ldap php5.6-zip zip git -y
+add-apt-repository ppa:ondrej/php; apt-get update; apt-get install php5.6-pgsql postgresql apache2 libgda-5.0-postgres   postgresql-common  postgresql-client-common libpg-perl postgresql postgresql-client php5.6 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-pgsql php5.6-xsl  php5.6-imap php5.6-sqlite3 php5.6-ldap php5.6-zip zip git -y
 #
 cd $LOCAL
 pwd
 
-#echo  "Comienza la descarga del repositorio $REPOSITORIO"
-#git clone $REPOSITORIO
+echo  "Comienza la descarga del repositorio $REPOSITORIO"
+git clone $REPOSITORIO
 
 echo  "A continuacion se va a crear la base de datos"
 sleep 3
