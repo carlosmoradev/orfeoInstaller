82433481d46739f16970799daa96b6cb160c90c5
Carlos Mario Mora Restrepo <contacto@carlosmora.biz>
2017-01-07 12:58:27 -0500

    Depuracion del proceso completo de instalacion

--- instalaOrfeo.sh
+++ instalaOrfeo.sh
@@ -5,38 +5,39 @@
 
 ###############################################################################
 
-REPOSITORIO="git@github.com:cmmora/orfeo384.git"    #Repositorio github
+REPOSITORIO="https://github.com/cmmora/orfeo384.git"    #Repositorio github
 LOCAL="/var/www/html"
-DBNAME="orfeo384"
+DBNAME="dborfeo384"
 DBUSER="orfeo_user"
 DBPASSWORD="0rf30**$$"
-INSTALLDIR="$LOCAL/instalacion"
+INSTALLDIR="$LOCAL/orfeo384/instalacion"
 PHPDIR="/etc/php/5.6/apache2"
 ###############################################################################
-apt-get update
-apt-get upgrade -y
+#apt-get update
+#apt-get upgrade -y
 
-add-apt-repository ppa:ondrej/php; apt-get update; apt-get install php5.6-pgsql postgresql apache2 libgda-5.0-postgres   postgresql-common  postgresql-client-common libpg-perl postgresql postgresql-client php5.6 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-pgsql php5.6-xsl  php5.6-imap php5.6-sqlite3 php5.6-ldap php5.6-zip zip git -y
-
+#add-apt-repository ppa:ondrej/php; apt-get update; apt-get install php5.6-pgsql postgresql apache2 libgda-5.0-postgres   postgresql-common  postgresql-client-common libpg-perl postgresql postgresql-client php5.6 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-pgsql php5.6-xsl  php5.6-imap php5.6-sqlite3 php5.6-ldap php5.6-zip zip git -y
+#
 cd $LOCAL
+pwd
 
-echo -e "Comienza la descarga del repositorio $REPOSITORIO"
-git $REPOSITORIO
+#echo  "Comienza la descarga del repositorio $REPOSITORIO"
+#git clone $REPOSITORIO
 
-echo -e "A continuacion se va a crear la base de datos"
+echo  "A continuacion se va a crear la base de datos"
 sleep 3
-
+cd /tmp
 sudo -u postgres psql -c "CREATE USER $DBUSER;"
 sudo -u postgres psql -c "alter user $DBUSER with password '$DBPASSWORD';"
-sudo -u postgres psql -c "CREATE DATABASE $DNAME WITH OWNER $DBUSER;"
+sudo -u postgres psql -c "CREATE DATABASE $DBNAME WITH OWNER $DBUSER;"
 
-echo -e "Cargando la base de datos base "
+echo  "Cargando la base de datos base "
 sleep 3
 
 sudo -u postgres psql -c "\c $DBNAME;"
-sudo -u postgres psql -c "\i $INSTALLDIR/orfeo-384-estructura.sql;"
+sudo -u postgres psql -c "\i $INSTALLDIR/dborfeo384.sql;"
 sudo -u postgres psql -c "update usuario set usua_nuevo=0 where usua_login='ADMON';"
 
 cat $INSTALLDIR/phpBase.ini > $PHPDIR/php.ini; service apache2 restart
-clear
-echo -e "Instalacion de orfeo Finalizada."
+#clear
+echo  "Instalacion de orfeo Finalizada."
