5079422cb39d5a159c182668fa0a24d26a766bd8
Carlos Mario Mora Restrepo <contacto@carlosmora.biz>
2017-01-08 09:44:59 -0500

    Asignacion de permisos a www-data

--- instalaOrfeo.sh
+++ instalaOrfeo.sh
@@ -13,6 +13,7 @@ DBPASSWORD="0rf30**$$"
 INSTALLDIR="$LOCAL/orfeo384/instalacion"
 PHPDIR="/etc/php/5.6/apache2"
 POSTGRESQLDIR="/etc/postgresql/9.5/main"
+
 ###############################################################################
 echo
 echo
@@ -20,31 +21,37 @@ cat $(pwd)/advertencia.txt
 
 
 
-#apt-get update
-#apt-get upgrade -y
-#
-#add-apt-repository ppa:ondrej/php; apt-get update; apt-get install php5.6-pgsql postgresql apache2 libgda-5.0-postgres   postgresql-common  postgresql-client-common libpg-perl postgresql postgresql-client php5.6 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-pgsql php5.6-xsl  php5.6-imap php5.6-sqlite3 php5.6-ldap php5.6-zip zip git -y
-##
-#cd $LOCAL
+apt-get update
+apt-get upgrade -y
+
+add-apt-repository ppa:ondrej/php; apt-get update; apt-get install php5.6-pgsql postgresql apache2 libgda-5.0-postgres   postgresql-common  postgresql-client-common libpg-perl postgresql postgresql-client php5.6 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-pgsql php5.6-xsl  php5.6-imap php5.6-sqlite3 php5.6-ldap php5.6-zip zip git -y
+
+cd $LOCAL
 #pwd
-#
-#echo  "Comienza la descarga del repositorio $REPOSITORIO"
-#git clone $REPOSITORIO
-#
-#echo  "A continuacion se va a crear la base de datos"
-#sleep 3
-#cd /tmp
-#sudo -u postgres psql -c "CREATE USER $DBUSER;"
-#sudo -u postgres psql -c "alter user $DBUSER with password '$DBPASSWORD';"
-#sudo -u postgres psql -c "CREATE DATABASE $DBNAME WITH OWNER $DBUSER;"
-#
-#echo  "Cargando la base de datos base "
-#sleep 3
-#
-#sudo -u postgres psql $DBNAME -c "\i $INSTALLDIR/dborfeo384.sql;"
-#sudo -u postgres psql $DBNAME -c "update usuario set usua_nuevo=0 where usua_login='ADMON';"
-#
-#cat $INSTALLDIR/phpBase.ini > $PHPDIR/php.ini; /etc/init.d/apache2 restart
-#cat $INSTALLDIR/pg_hba.conf > $POSTGRESQLDIR/pg_hba.conf; /etc/init.d/postgresql restart
-##clear
-#echo  "Instalacion de orfeo Finalizada."
+
+echo  "Comienza la descarga del repositorio $REPOSITORIO"
+git clone $REPOSITORIO
+sleep 2
+chown www-data:www-data $REPOSITORIO -Rv
+cp index.html index.html.preOrfeo
+
+echo  "A continuacion se va a crear la base de datos"
+sleep 3
+cd /tmp
+sudo -u postgres psql -c "CREATE USER $DBUSER;"
+sudo -u postgres psql -c "alter user $DBUSER with password '$DBPASSWORD';"
+sudo -u postgres psql -c "CREATE DATABASE $DBNAME WITH OWNER $DBUSER;"
+
+echo  "Cargando la base de datos base "
+sleep 3
+
+sudo -u postgres psql $DBNAME -c "\i $INSTALLDIR/dborfeo384.sql;"
+sudo -u postgres psql $DBNAME -c "update usuario set usua_nuevo=0 where usua_login='ADMON';"
+
+cp $PHPDIR/php.ini $PHPDIR/php.ini.preOrfeo
+cat $INSTALLDIR/phpBase.ini > $PHPDIR/php.ini; /etc/init.d/apache2 restart
+
+cp $POSTGRESQLDIR/pg_hba.conf	$POSTGRESQLDIR/pg_hba.conf.preOrfeo
+cat $INSTALLDIR/pg_hba.conf > $POSTGRESQLDIR/pg_hba.conf; /etc/init.d/postgresql restart
+#clear
+echo  "Instalacion de orfeo Finalizada."
