d636b1e006e9c68d20f07489068dc93e7aa5e47f
Carlos Mario Mora Restrepo <contacto@carlosmora.biz>
2017-01-08 21:43:42 -0500

    advertencia verificada

--- instalaOrfeo.sh
+++ instalaOrfeo.sh
@@ -41,7 +41,6 @@ apt-get upgrade -y
 add-apt-repository ppa:ondrej/php; apt-get update; apt-get install php5.6-pgsql postgresql apache2 libgda-5.0-postgres   postgresql-common  postgresql-client-common libpg-perl postgresql postgresql-client php5.6 libapache2-mod-php5.6 php5.6-curl php5.6-gd php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-xmlrpc php5.6-pgsql php5.6-xsl  php5.6-imap php5.6-sqlite3 php5.6-ldap php5.6-zip zip git -y
 
 cd $LOCAL
-#pwd
 
 echo  "Comienza la descarga del repositorio $REPOSITORIO"
 git clone $REPOSITORIO
